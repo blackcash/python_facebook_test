8d9859910743bad6ac667ff1178c1bd2b77a0594
blackcash <jht325@hotmail.com>
2017-11-27 14:39:39 +0800

    origin

---
+++ README.md
@@ -0,0 +1,13 @@
+#使用python讀取facebook文章中的按讚數,並將從中抽出有留言和按讚的人
+
+1.取得token和要抽取出來的文章<br>
+(1)連上facebook開發者平台(https://developers.facebook.com/tools-and-support/)<br>
+(2)選取"圖形 API 測試工具",即可取得token <br>
+(3)在命令列打"me/posts"即可找出自己所po的文章,在從中找出文章的id <br>
+<br>
+2.安裝facebook sdk: pip install facebook-sdk<br>
+<br>
+3.程式:<br>
+(1)匯入要使用的facebook api : import facebook  <br>
+(2)建構使用的物件:graph = facebook.GraphAPI(access_token=token,version=version)
+(3)使用物件取得資料(取得有留言且限制1000筆):graph.get_object(id=object_id+'?fields=comments.limit(1000)')
---
+++ facebook_test.py
@@ -0,0 +1,37 @@
+import facebook
+import random
+
+token = ''  # 所使用的token
+version = '2.7'  # 使用的API版本
+graph = facebook.GraphAPI(access_token=token,version=version)
+object_id = ""  # 要抓取的文章ID
+
+#取得按讚的人數
+list_likes = []
+post = graph.get_object(id=object_id+'?fields=likes.limit(1000)')
+likes = post['likes']['data']
+print ('共有'+str(len(likes))+'按讚')
+for like in likes:
+    list_likes.append(like['name'])       
+print(list_likes)
+
+#取得有留言的人數
+list_comments = []
+post = graph.get_object(id=object_id+'?fields=comments.limit(1000)')
+comments = post['comments']['data']
+print ('共有'+str(len(comments))+'留言')
+for comment in comments:
+    list_comments.append(comment['from']['name'])
+print (list_comments)
+
+#取得有留言和按讚的人數
+list_people = []
+for p in list_likes:
+    if p in list_comments:
+        list_people.append(p)        
+print ('共有'+str(len(list_people))+'留言和按讚')
+print (list_people)
+
+#隨機抽出有留言和按讚的人  
+no = random.randint(0,len(list_people)-1)
+print("恭喜 {}  抽中!".format(list_people[no]))
