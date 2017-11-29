10ccf76c4b97b57b3097d9daf802d7f5e0a70319
blackcash <jht325@hotmail.com>
2017-11-29 15:44:51 +0800

    判斷是否有存在的字典資料

--- facebook_getGroupPicture.py
+++ facebook_getGroupPicture.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from dateutil.parser import parse
 import facebook
 import shutil
+import simplejson
 
 group_id = input("請輸入社團的ID:")
 token =  input("請輸入使用的token:")
@@ -23,16 +24,26 @@ for post in res.json()['data']:
     #posts.append([parse(post.get('created_time')[0:19]), post.get('id'), post.get('message'), post.get('story')])
     posts.append([parse(post.get('created_time')[0:19]), post.get('id'), post.get('message')])
     object_id = post.get('id')
-    pic = graph.get_object(id=object_id+'?fields=full_picture,picture')
     try:
-        print (pic['full_picture'])
-        res = requests.get(pic['full_picture'] , stream = True)
-        f = open("B"+post.get('id')+".jpg",'wb')
-        shutil.copyfileobj(res.raw,f)
-        print (pic['picture'])
-        res = requests.get(pic['picture'] , stream = True)
-        f = open("S"+post.get('id')+".jpg",'wb')
-        shutil.copyfileobj(res.raw,f)
+        picb = graph.get_object(id=object_id+'?fields=full_picture')
+        print (type(picb),picb)
+        if 'full_picture' in picb.keys():
+            print (picb['full_picture'])
+            res = requests.get(picb['full_picture'] , stream = True)
+            f = open("B"+post.get('id')+".jpg",'wb')
+            shutil.copyfileobj(res.raw,f)
+        else:
+            print(object_id+"沒有大圖片資料")
+
+        pics = graph.get_object(id=object_id+'?fields=picture')    
+        print (type(pics),pics)
+        if 'picture' in pics.keys():	   
+            print (pics['picture'])
+            res = requests.get(pics['picture'] , stream = True)
+            f = open("S"+post.get('id')+".jpg",'wb')
+            shutil.copyfileobj(res.raw,f)
+        else:
+            print(object_id+"沒有小圖片資料")
     except:
         print(object_id+"沒有圖片資料")
 
